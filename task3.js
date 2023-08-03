d1c631a65ad575c4362b06a2f24f4f45060d0318
Mikoguu <86719893+Mikoguu@users.noreply.github.com>
2023-08-03 14:47:36 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,7 +1,5 @@
 const path = "/users/download/index.html";
-const fileExtension = '.html';
 
-const isHtml = path => path.includes(fileExtension);
+const isHtml = path => path.length - path.indexOf('.html') == 5;
 
 console.log(isHtml(path));
-     
